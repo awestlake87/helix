4a4d484ac481c415f392d52403740236a89ae893
Andrew J Westlake <kelixes@gmail.com>
2017-02-14 18:21:10 -0600

    added basic error infrastructure to frontend

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
+#include <meta/err/index.hpp>
+
 #include <argp.h>
+
+using namespace Meta;
 
 static int parse_opt(int key, char* arg, struct argp_state* state) {
   switch (key) {
@@ -18,23 +22,40 @@ static int parse_opt(int key, char* arg, struct argp_state* state) {
 }
 
 int main(int argc, char** argv) {
-  //llvm::InitializeAllTargetInfos();
-  //llvm::InitializeAllTargets();
-  //llvm::InitializeAllTargetMCs();
-  //llvm::InitializeAllAsmParsers();
-  //llvm::InitializeAllAsmPrinters();
-
-  struct argp_option options[] = {
-    { "output", 'o', "PATH", 0, "path to output executable [TEMPORARY]" },
-    { 0 }
+  auto _handleError = [](ErrorCode ec, const char* msg) -> int {
+    int ec_int = static_cast<int>(ec);
+    std::cerr << '[' << ec_int << "]: " << msg << std::endl;
+    return ec_int;
   };
-  struct argp argp_instance = { options, parse_opt, "PATH" };
 
-  int result = argp_parse(&argp_instance, argc, argv, 0, 0, 0);
+  try {
+    //llvm::InitializeAllTargetInfos();
+    //llvm::InitializeAllTargets();
+    //llvm::InitializeAllTargetMCs();
+    //llvm::InitializeAllAsmParsers();
+    //llvm::InitializeAllAsmPrinters();
 
-  if (result == 0) {
-    //s_Compiler->compile();
+    struct argp_option options[] = {
+      { "output", 'o', "PATH", 0, "path to output executable [TEMPORARY]" },
+      { 0 }
+    };
+    struct argp argp_instance = { options, parse_opt, "PATH" };
+
+    int result = argp_parse(&argp_instance, argc, argv, 0, 0, 0);
+
+    if (result != 0) {
+      throw InvalidCommandLineArgsError("invalid command line arguments");
+    }
+
+    return result;
   }
-
-  return result;
+  catch (Exception& e) {
+    return _handleError(e.getCode(), e.what());
+  }
+  catch (std::exception& e) {
+    return _handleError(ErrorCode::UNKNOWN, e.what());
+  }
+  catch (...) {
+    return _handleError(ErrorCode::UNKNOWN, "an unknown error occurred");
+  }
 }
