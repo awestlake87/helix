f9ed54c8437617a0382e4006012862154656da33
awestlake87 <kelixes@gmail.com>
2017-02-23 02:00:14 -0600

    added static linkage for functions

--- test.sh
+++ test.sh
@@ -7,11 +7,6 @@ echo -e "\033[0;34m---x[  SUPER FANCY TEST SUITE BEGIN  ]x---\033[0m"
 
 function meta_test {
   local DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-  
-  if [ "$1" == "" ]; then
-    echo "no tests to run"
-    return 0
-  fi
 
   function echo_fail {
     echo -e "\033[1;31m[FAIL]\033[0m $@"
@@ -49,6 +44,11 @@ function meta_test {
 
   return 0
 }
+
+rm -rf $DIR/tests/compile-unit/*.out
+rm -rf $DIR/tests/compile-unit/*.o
+rm -rf $DIR/tests/compile-unit/*.ll
+
 # run meta_test for *.meta recursively in ./test
 export -f meta_test
 find "$DIR/tests/compile-unit" -name "*.meta" | xargs -n 1 bash -c 'meta_test "$@"' _
